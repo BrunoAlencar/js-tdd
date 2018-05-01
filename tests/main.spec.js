bf452810f5cac68539a3486e875c2871e6766bb5
Bruno Alencar <joaobrunoalencar@gmail.com>
2018-05-01 12:03:08 -0300

    using expect of chai

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,45 @@
+var expect = require('chai').expect;
+
 describe('Main', () => {
-  describe('Method A', () => {
-    context('this do blabla', () => {
-      it('should happen bla bla', () => {
-      });
-    });
-  });
+  var arr;
+
+  before(() => {
+    console.log('Before')
+  })
+
+  after(() => {
+    console.log('After')
+  })
+
+  beforeEach(() => {
+    arr = [1, 2, 3, 4];
+  })
+
+  afterEach(() => {
+    console.log('after each')
+  })
+
+  it('should be an array', ()=>{
+    expect(arr).to.be.an('array')
+  })
+
+  it('should remove a item of array', () => {
+    arr.pop();
+    expect(arr).to.have.lengthOf(3);
+  })
+
+  it('should remove the value of 3', () => {
+    arr.splice(1,1)
+    expect(arr).to.not.include(2)
+  })
+
+  it('should return true when pop 3 from the array', () => {
+    arr.splice(2,1)
+    console.log(arr)
+    expect(arr.indexOf(3) === -1).to.be.true;
+  })
+
+
+
+
 });
