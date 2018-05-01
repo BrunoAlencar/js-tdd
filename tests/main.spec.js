c5be6fdeeabe838f81107b348e3ca8a6ecfe7838
Bruno Alencar <joaobrunoalencar@gmail.com>
2018-05-01 12:39:46 -0300

    lib de calculcar

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,45 +1,70 @@
 var expect = require('chai').expect;
 
-describe('Main', () => {
-  var arr;
+var calc = require('../src/main.js')
 
-  before(() => {
-    console.log('Before')
-  })
+describe('Calc', () => {
 
-  after(() => {
-    console.log('After')
-  })
+    // smoke tests
 
-  beforeEach(() => {
-    arr = [1, 2, 3, 4];
-  })
+    describe('Smoke tests', () => {
 
-  afterEach(() => {
-    console.log('after each')
-  })
+        it('shoulkd exist the calc lib', () => {
+            expect(calc).to.exist;
+        })
 
-  it('should be an array', ()=>{
-    expect(arr).to.be.an('array')
-  })
 
-  it('should remove a item of array', () => {
-    arr.pop();
-    expect(arr).to.have.lengthOf(3);
-  })
+        it('should exist the method `sum`', () => {
+            expect(calc.sum).to.exist;
+            expect(calc.sum).to.be.a('function');
+        })
 
-  it('should remove the value of 3', () => {
-    arr.splice(1,1)
-    expect(arr).to.not.include(2)
-  })
+        it('should exist the method `sub`', () => {
+            expect(calc.sub).to.exist;
+            expect(calc.sub).to.be.a('function');
+        })
 
-  it('should return true when pop 3 from the array', () => {
-    arr.splice(2,1)
-    console.log(arr)
-    expect(arr.indexOf(3) === -1).to.be.true;
-  })
+        it('should exist the method `mult`', () => {
+            expect(calc.mult).to.exist;
+            expect(calc.mult).to.be.a('function');
+        })
+
+        it('should exist the method `div`', () => {
+            expect(calc.div).to.exist;
+            expect(calc.div).to.be.a('function');
+        })
 
 
 
+    })
+    describe('Sum', () => {
+        it('should return 4 when `sum(2,2)`', () => {
+            expect(calc.sum(2,2)).to.be.equal(4)
+        })
+    })
+
+    describe('Sub', () => {
+        it('should return 0 when `sub(2,2)`', () => {
+            expect(calc.sub(2,2)).to.be.equal(0)
+        })
+        it('should return -4 when `sub(6,10)`', () => {
+            expect(calc.sub(6,10)).to.be.equal(-4)
+        })
+    })
+
+    describe('Mult', () => {
+        it('should return 4 when `sum(2,2)`', () => {
+            expect(calc.mult(2,2)).to.be.equal(4)
+        })
+    })
+
+    describe('Div', () => {
+        it('should return 1 when `sum(2,2)`', () => {
+            expect(calc.div(2,2)).to.be.equal(1)
+        })
+
+        it('should return `Não é possivel divisão por zero!` when divide by 0', () => {
+            expect(calc.div(2,0)).to.be.equal('Não é possivel divisão por zero!')
+        })
+    })
 
 });
